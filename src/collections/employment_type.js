8f1b77b8064800ef77e8e7dbf1632facf4a501d3
Rubens Gomes <vitorr_gomes@hotmail.com>
2018-01-30 12:41:49 -0200

    update employment collections

--- src/collections/employment_type.js
+++ src/collections/employment_type.js
@@ -1,38 +1,20 @@
+// mapeada igual o app 30/01/18
 const employment_type_map = {
-  "CO": { "code": "CO", "label": "Contratado" },
+  "CO": { "code": "CO", "label": "Período Integral" },
+  "IN": { "code": "IN", "label": "Meio Período" },
+  "TC": { "code": "TC", "label": "Terceiro" },
   "TE": { "code": "TE", "label": "Temporário" },
   "VO": { "code": "VO", "label": "Voluntário" },
-  "TR": { "code": "TR", "label": "Trainee" },
-  "TC": { "code": "TC", "label": "Terceiro" },
-  "RE": { "code": "RE", "label": "Aposentado" },
-  "SE": { "code": "SE", "label": "Autônamo" },
-  "FO": { "code": "FO", "label": "Estrangeiro" },
-  "IN": { "code": "IN", "label": "Estagiário" },
-  "MA": { "code": "MA", "label": "Menor Aprendiz" },
-  "DI": { "code": "DI", "label": "PCD" },
-  "PA": { "code": "PA", "label": "Sócio" }
+  "OT": { "code": "OT", "label": "Outros" }
 };
 
 const employment_type_legal_map = {
-  "MA": { "code": "MA", "label": "Aprendizagem - Contrato de aprendizagem (Menor Aprendiz)" },
-  "ES": { "code": "ES", "label": "Estágio - Contrato de estágio (Estágiário)" },
-  "PJ": { "code": "PJ", "label": "PJ - Pessoa Jurídica" },
-  "AU": { "code": "AU", "label": "Autônomo" },
-  "BO": { "code": "BO", "label": "Bolsista" },
-  "SO": { "code": "SO", "label": "Societário" },
-  "ST": { "code": "ST", "label": "Estatuário" },
-  "OO": { "code": "OO", "label": "Cooperativa" },
+  "MA": { "code": "MA", "label": "Menor Aprendiz" },
+  "ES": { "code": "ES", "label": "Estágio" },
   "CL": { "code": "CL", "label": "CLT - Prazo indeterminado" },
-  "CD": { "code": "CD", "label": "CLT - Prazo determinado - Contrato por obra certa" },
-  "CS": { "code": "CS", "label": "CLT - Prazo determinado - Contrato por serviço determinado" },
-  "CT": { "code": "CT", "label": "CLT - Prazo determinado - Contrato Temporário" },
-  "CO": { "code": "CO", "label": "CLT Cotas" },
-  "CF": { "code": "CF", "label": "CLT - Flex" },
-  "MI": { "code": "MI", "label": "MEI - Contrato com Micro Empreendedor Individual" },
-  "RP": { "code": "RP", "label": "RPA - Contrato com Recibo de Pagamento Autônomo" },
-  "NC": { "code": "NC", "label": "Sem Contrato" },
-  "TE": { "code": "TE", "label": "Terceirizado" },
-  "VO": { "code": "VO", "label": "Voluntário" }
+  "PJ": { "code": "PJ", "label": "PJ - Pessoa Jurídica" },
+  "SO": { "code": "SO", "label": "Societário" },
+  "ST": { "code": "ST", "label": "Estatuário" }
 };
 
 const employment_type_collection = Object.keys(employment_type_map).map(
